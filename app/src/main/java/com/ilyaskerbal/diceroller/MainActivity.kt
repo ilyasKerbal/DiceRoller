8e6b5e7f48e51949390c03a1b3bef4734ceb215e
ILYAS KERBAL <kerbalsc@gmail.com>
2022-03-15 23:24:19 +0100

    onClickListener added to the button

--- app/src/main/java/com/ilyaskerbal/diceroller/MainActivity.kt
+++ app/src/main/java/com/ilyaskerbal/diceroller/MainActivity.kt
@@ -3,11 +3,15 @@ package com.ilyaskerbal.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var roll_button : Button = findViewById(R.id.roll_button);
+        val roll_button : Button = findViewById(R.id.roll_button);
+        roll_button.setOnClickListener {
+            Toast.makeText(this, "Button Tapped!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
