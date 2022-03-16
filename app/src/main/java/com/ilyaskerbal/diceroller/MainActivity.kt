81261cfef112972b1fd2eeef033ea83764f2ef11
ILYAS KERBAL <kerbalsc@gmail.com>
2022-03-16 18:16:05 +0100

    Adding dice images and changing dice image when roll is tapped

--- app/src/main/java/com/ilyaskerbal/diceroller/MainActivity.kt
+++ app/src/main/java/com/ilyaskerbal/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ilyaskerbal.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import java.util.*
 
@@ -17,8 +18,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val textResult : TextView = findViewById(R.id.text_result)
-        val randomInt = Random().nextInt(6) + 1
-        textResult.text = randomInt.toString()
+        val diceImage : ImageView = findViewById(R.id.dice_image)
+        val diceDrawable = when(Random().nextInt(6) + 1){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice
+        }
+        diceImage.setImageResource(diceDrawable)
     }
 }
